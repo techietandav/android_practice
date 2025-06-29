41d10179bdeca1e65ae443e2b7c986608a3747a7
Rohan Singh <rohansingh191996@gmail.com>
2025-06-30 00:45:11 +0530

    second react native practice

--- App.tsx
+++ App.tsx
@@ -1,101 +1,139 @@
-//import { Text, View,SafeAreaView, TouchableOpacity, Alert, StyleSheet, Pressable } from 'react-native';
-import { View, StyleSheet, ScrollView } from 'react-native';
+import { FlatList, View, Text, Image, StyleSheet } from 'react-native';
 import React from 'react';
-
+const dummy = [
+  {
+    id: 1,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 2,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 3,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 4,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 5,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 6,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 7,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 8,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 9,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 10,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 11,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 12,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 13,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+  {
+    id: 14,
+    name: 'Muskan',
+    email: 'muskan123',
+    image: './assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg',
+  },
+];
 const App = () => {
   return (
-    <ScrollView contentContainerStyle={{gap:10}} horizontal style={styles.container}>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-      <View style={styles.box1}/>
-      <View style={styles.box2}/>
-      <View style={styles.box3}/>
-
-
-      {/* <Text style={styles.text}>Hello Rohan Singh</Text>
-
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => Alert.alert('Pressed!!')}
-      >
-        <Text style={styles.buttonText}>Button</Text>
-      </TouchableOpacity>
-      <Pressable style={{padding:10,backgroundColor:"yellow"}}>
-        <Text>Press Me</Text>
-      </Pressable> */}
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={dummy}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image
+              source={require('./assets/images/kevin-mueller-QGSrJHopKwY-unsplash.jpg')}
+              style={styles.image}
+            />
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+              <Text>name:</Text>
+              <Text>email:</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+        ItemSeparatorComponent={<View style={{ height: 10 }} />}
+        numColumns={4}
+        columnWrapperStyle={{ gap: 10 }}
+      />
+      <Text>App</Text>
+    </View>
   );
 };
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
-    padding:10,
-    // flexDirection:"row",
-    // justifyContent:"center",
-    // alignItems:"center",
-    // justifyContent: 'center', // Vertically center content
-    // alignItems: 'center', // Horizontally center content
-    // paddingTop: 20, // Add space from top if needed
+    backgroundColor: '#dadada',
+    paddingVertical: 10,
+    paddingHorizontal: 5,
   },
-  box1:{
-    // flex:1,
-    width:80,
-    height:80,
-    backgroundColor:'red',
-    borderRadius:50,
+  card: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  box2:{
-    // flex:2,
-    width:80,
-    height:80,
-    backgroundColor:'yellow',
-    borderRadius:50,
-  },
-  box3:{
-    // flex:3,
-    width:80,
-    height:80,
-    backgroundColor:'blue',
-    borderRadius:50,
-  },
-
-  text: {
-    fontSize: 20,
-    marginBottom: 20, // Space between text and button
-  },
-  button: {
-    padding: 10,
-    backgroundColor: 'green',
-    borderRadius: 5, // Optional: To make button corners rounded
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 16,
+  image: {
+    width: 40,
+    height: 40,
+    borderRadius: 50,
   },
 });
-
-export default App;
